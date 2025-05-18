689fd30924d55b243c8543fb1453460f9007b6cb
eydanlavi <eydanlavi@gmail.com>
2025-05-18 18:10:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 general_classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 suicide_classifier = pipeline("text-classification", model="vibhorag101/roberta-base-suicide-prediction-phr", top_k=None)
 
-text = "i just missed the game winner for my last playoff basketball game. i want to kill myself"
+text = ""
 res1 = general_classifier(text)
 res2 = suicide_classifier(text)
 
